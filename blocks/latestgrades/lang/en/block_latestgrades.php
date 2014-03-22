e4a08dfd1f30623238f5e1e2259fe1d522dd94ec
Nawshad Farruque <me.naws@gmail.com>
2014-03-22 15:59:26 -0400

    Nawshad-Moodle26-LatestGrades Block Added

---
+++ blocks/latestgrades/lang/en/block_latestgrades.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Latest Grades';
+$string['latestgrades:addinstance'] = 'Add a new Latest Grades block';
+$string['latestgrades:myaddinstance'] = 'Add a new Latest Grades block to the My Moodle page';
+
+?>
---
+++ blocks/latestgrades/version.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2013050523;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->requires = 2012112900; // YYYYMMDDHH (This is the release version for Moodle 2.0)
+$plugin->component = 'block_latestgrades';
+
+?>
