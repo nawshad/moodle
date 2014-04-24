90781adb368ffc0ce100f572edd9e63cb26ac27c
Nawshad Farruque <farnaws@lambdasolutions.net>
2014-04-24 15:00:52 -0700

    Changed Items from navbar-inner and added asm theme

--- theme/asm/layout/columns3.php
+++ theme/asm/layout/columns3.php
@@ -1,4 +1,5 @@
 <?php
+include_once('globals.php');
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -15,7 +16,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Moodle's Reca Clean theme, an example of how to make a Bootstrap theme
+ * Moodle's Clean theme, an example of how to make a Bootstrap theme
  *
  * DO NOT MODIFY THIS THEME!
  * COPY IT FIRST, THEN RENAME THE COPY AND MODIFY IT INSTEAD.
@@ -23,16 +24,13 @@
  * For full information about creating Moodle themes, see:
  * http://docs.moodle.org/dev/Themes_2.0
  *
- * @package   theme_reca_clean
+ * @package   theme_clean
  * @copyright 2013 Moodle, moodle.org
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 // Get the HTML for the settings bits.
-
-
-$html = theme_reca_clean_get_html_for_settings($OUTPUT, $PAGE);
-$hasfootnote = (!empty($PAGE->theme->settings->footnote));
+$html = theme_asm_get_html_for_settings($OUTPUT, $PAGE);
 
 if (right_to_left()) {
     $regionbsid = 'region-bs-main-and-post';
@@ -40,11 +38,15 @@ if (right_to_left()) {
     $regionbsid = 'region-bs-main-and-pre';
 }
 
+$custommenu = $OUTPUT->custom_menu();
+$hascustommenu = (empty($PAGE->layout_options['nocustommenu']) && !empty($custommenu));
+
 echo $OUTPUT->doctype() ?>
 <html <?php echo $OUTPUT->htmlattributes(); ?>>
 <head>
-    <title><?php echo $OUTPUT->page_title();?></title>
+    <title><?php echo $OUTPUT->page_title(); ?></title>
     <link rel="shortcut icon" href="<?php echo $OUTPUT->favicon(); ?>" />
+    <link href="http://fonts.googleapis.com/css?family=Oswald" rel="stylesheet" type="text/css">
     <?php echo $OUTPUT->standard_head_html() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
@@ -52,35 +54,38 @@ echo $OUTPUT->doctype() ?>
 <body <?php echo $OUTPUT->body_attributes(); ?>>
 
 <?php echo $OUTPUT->standard_top_of_body_html() ?>
-<header role="banner" class="navbar navbar-fixed-top<?php echo $html->navbarclass ?>">    
-    <nav role="navigation" class="navbar-inner">    
-        <div class="container-fluid"> 
-            <a class="brand" href="<?php echo $CFG->wwwroot;?>"><?php echo $SITE->shortname; ?></a>
+<header role="banner" class="navbar">
+    <nav role="navigation" class="navbar-inner">
+        <div class="bluestipe">
+            <div class="container-fluid top_stripe"><?php echo $OUTPUT->login_info() ?></div>
+        </div>
+        <div class="container-fluid">
+            <a class="brand" href="<?php echo $CFG->wwwroot;?>"><?php echo $global_logo; ?></a>
+            <?php if ($hascustommenu) { ?>
             <a class="btn btn-navbar" data-toggle="workaround-collapse" data-target=".nav-collapse">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </a>
-            <div class="nav-collapse collapse">
-                <?php echo $OUTPUT->custom_menu(); ?>
-                <ul class="nav pull-right">
-                    <li><?php echo $OUTPUT->page_heading_menu(); ?></li>
-                    <li class="navbar-text"><?php echo $OUTPUT->login_info() ?></li>
-                </ul>
-            </div>
+            <?php } ?>            
         </div>
     </nav>
 </header>
-
-<div id="page" class="container-fluid">
-    <header id="page-header" class="clearfix">
-        <div id = "logo_c3" class = "logo">
-            <img src="<?php echo $OUTPUT->pix_url('reca_logo','theme')?>"  alt="Moodle logo" />
+<?php if ($hascustommenu) { ?>
+<div class="navbar topmenu ">
+    <div class="container-fluid">
+        <div class="nav-collapse collapse">                                
+            <?php echo $OUTPUT->custom_menu(); ?>
         </div>
-    
-        <div id="page-navbar" class="clearfix">
-            <nav class="breadcrumb-nav"><?php echo $OUTPUT->navbar(); ?></nav>
-            <div class="breadcrumb-button"><?php echo $OUTPUT->page_heading_button(); ?></div>
+    </div>
+</div>
+<?php } ?>
+<div id="page" class="container-fluid">
+
+    <header id="page-header" class="clearfix">
+        <div id="page-navbar">
+            <nav class="breadcrumb-button"><?php echo $OUTPUT->page_heading_button(); ?></nav>
+            <?php echo $OUTPUT->navbar(); ?>
         </div>
         <?php echo $html->heading; ?>
         <div id="course-header">
@@ -105,22 +110,18 @@ echo $OUTPUT->doctype() ?>
     </div>
 
     <footer id="page-footer">
-        <div class="footer-left">
-              <a href="http://moodle.org" title="Moodle">
-                <img src="<?php echo $OUTPUT->pix_url('moodle-logo','theme')?>" alt="Moodle logo" />
-              </a>                   
-        </div>
-        <?php if ($hasfootnote) { ?>
-                    
-        <?php } ?> 
+        <div id="course-footer"><?php echo $OUTPUT->course_footer(); ?></div>
+        <p class="helplink"><?php echo $OUTPUT->page_doc_link(); ?></p>
+        <?php
+        echo $html->footnote;
+        echo $OUTPUT->login_info();
+        echo $OUTPUT->home_link();
+        echo $OUTPUT->standard_footer_html();
+        ?>
+    </footer>
 
-        <div class="footer-right">
-            <?php echo $OUTPUT->login_info();?>
-           
-        </div>
-        <?php echo $OUTPUT->standard_footer_html(); ?>
-    </footer> 
     <?php echo $OUTPUT->standard_end_of_body_html() ?>
+
 </div>
 </body>
 </html>
