03797bd796e1b8c6e22807518bac27d75189bc96
Nawshad Farruque <me.naws@gmail.com>
2014-04-28 00:51:59 -0700

    Deleted reca_clean, reca, asm, added new reca theme

--- theme/asm/layout/globals.php
+++
@@ -1,2 +0,0 @@
-<?php
-$global_logo='<img title="ASM International" alt="ASM International" src="'.$OUTPUT->pix_url('asm_logo', 'theme').'"/>';
---
+++ theme/reca/layout/globals.php
@@ -0,0 +1,2 @@
+<?php
+$global_logo='<img title="RECA Logo" alt="RECA Logo" src="'.$OUTPUT->pix_url('reca_logo', 'theme').'"/>';
